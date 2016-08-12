e82deb121aaf7e30a22d78bc28837b6fc38632a4
vanAmsterdam <woutervanamsterdam@gmail.com>
2016-08-12 12:50:34 -0400

    subtle changes to enhance readability

--- run_analysis.R
+++ run_analysis.R
@@ -26,30 +26,30 @@ readinDir <- function(dir, recursive = T) {
 # Download and read in data -----------------------------------------------
 # url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 # download.file(url, destfile = "data/project_data.zip")
-## assuming there is a 'data' directory in you working directory which contains the needed data
+## assuming there is a 'data' directory in your working directory which contains the needed data
 activity_labels <- fread("data/UCI HAR Dataset/activity_labels.txt")
 features <- fread("data/UCI HAR Dataset/features.txt")
 readinDir('data/UCI HAR Dataset/train/', recursive = F)
 readinDir('data/UCI HAR Dataset/test/', recursive = F)
 features <- features[[2]]
 
-# rbind data with reference to partition
-df <- rbind(X_train[,partition:=factor(rep(1, .N), levels = c(1, 2), labels = c("train", "test"))], 
-           X_test[,partition:=factor(rep(2, .N), levels = c(1, 2), labels = c("train", "test"))]) 
-colnames(df) <- c(features, "partition")
+# rbind data
+df <- rbind(X_train, X_test)
+colnames(df) <- features
+## add factor variable for partition
+df[, partition:=factor(c(rep('train', nrow(X_train)), rep('test', nrow(X_test))))]
 table(df$partition) # check that the labeling of the partitioning worked out
 # add 'outcome' data (labels) with their nice names
 df[partition == 'train', activity:=factor(y_train$V1, levels = activity_labels$V1, labels = activity_labels$V2)]
 df[partition == 'test', activity:=factor(y_test$V1, levels = activity_labels$V1, labels = activity_labels$V2)]
-table(df$activity) # check labels
-# add subject
+table(df$activity) # check activity labels
+# add subject identifier
 subject <- rbind(subject_train, subject_test)
 df[, subject:=subject]
 
 # select features with mean or SD
 selFeatures <- grep("mean|std", features, value = T)
 df <- df[, c(selFeatures, "subject", "partition", "activity"), with = F]
-
 
 # Create second tidy dataset ----------------------------------------------
 df2 <- df[, lapply(.SD, mean), by = "activity,subject", .SDcols = selFeatures]
